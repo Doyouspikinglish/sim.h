d5223d473f6c71287e12498012e207bd79883ed3
Doyouspikinglish <kaanbekdas8@gmail.com>
2025-07-05 13:49:36 +0200

    Update sim.h
    deleted the $ signs for compability

--- sim.h
+++ sim.h
@@ -1,28 +1,34 @@
+#ifndef SIM_H
+#define SIM_H
+
+
 #include <iostream>
 #include <bitset>
 #include <bit>
 
 namespace sim {
 
-	void printbit$number(int a) {
+	void printbit_number(int a) {
 		std::bitset<8> bits(a);
 		std::cout << bits << " " << a << "\n";
 	}
 
-	void printbit_only$bin(int b) {
+	void printbit_only_bin(int b) {
 		std::bitset<8> bits_2(b);
 		std::cout << bits_2 << "\n";
 	}
 
-	void printbit_only$number(int c) {
+	void printbit_only_number(int c) {
 		std::bitset<8> bits_3(c);
 		std::cout << c << "\n";
 	}
 
-	void printbit_text$binary(const std::string& text) {
+	void printbit_text_binary(const std::string& text) {
 		for (char c : text) {
 			std::bitset<8> binary(c);
 			std::cout << binary << " ";
 		}
 	}
 }
+
+#endif
